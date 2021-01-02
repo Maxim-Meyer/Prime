42f93273616e3e0fcbfdcee2092ec9a8362853d2
Maxim Meyer <meyer.maxim@t-online.de>
2021-01-02 14:40:25 +0100

    Initial Commit

---
+++ prime/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  prime
+//
+//  Created by Maxim Meyer on 02.01.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
