85d1c270c7d367855102f5d6260a99537b8a6fd8
Maxim Meyer <meyer.maxim@t-online.de>
2021-02-21 21:33:00 +0100

    Initial commit

--- prime/AppDelegate.swift
+++ prime/AppDelegate.swift
@@ -1,26 +1,23 @@
 //
 //  AppDelegate.swift
-//  prime
+//  macos2
 //
-//  Created by Maxim Meyer on 02.01.21.
+//  Created by Maxim Meyer on 26.12.20.
 //
 
 import Cocoa
+import AVFoundation
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+ 
     
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        AudioServicesPlayAlertSound(SystemSoundID(1322))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
--- prime/ViewController.swift
+++ prime/ViewController.swift
@@ -1,14 +1,63 @@
 //
 //  ViewController.swift
-//  prime
+//  macos2
 //
-//  Created by Maxim Meyer on 02.01.21.
+//  Created by Maxim Meyer on 26.12.20.
 //
 
 import Cocoa
+import AppKit
+
+extension String
+{
+    func trim() -> String
+   {
+    return self.trimmingCharacters(in: NSCharacterSet.whitespaces)
+   }
+}
 
 class ViewController: NSViewController {
 
+    func isPrime(_ n: Int) -> Bool {
+        guard n >= 2     else { return false }
+        guard n != 2     else { return true  }
+        guard n % 2 != 0 else { return false }
+        return !stride(from: 3, through: Int(sqrt(Double(n))), by: 2).contains { n % $0 == 0 }
+    }
+    
+    func isInt(_ s: String)->Bool{
+        guard (Int(s) != nil) else { return false }
+        return true
+    }
+
+    @IBOutlet weak var prime_field: NSTextField!
+    @IBOutlet weak var label_field: NSTextField!
+    
+    @IBAction func check_prime(_ sender: NSButton) {
+        var name = prime_field.stringValue.trim()
+        //var myInt: Int
+        
+        if isInt(name) {
+            let myInt=Int(name)
+            if isPrime(myInt!){
+                    let greeting = String(myInt ?? 0)+" is a prime number!"
+                    label_field.stringValue = greeting
+                    NSSound.beep()
+                }else
+                {
+                    let greeting = String(myInt ?? 0)+" is not a prime number :("
+                    label_field.stringValue = greeting
+                    NSSound.beep()
+                }
+        }else
+        {
+            label_field.stringValue = String(name) + " is not a number. Please enter a number!"
+            NSSound.beep()
+        }
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +69,11 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        self.view.window?.title = "Prime checker"
+    }
+    
 }
 
